b62d15cd0f8a18efba1183c649ca217bce9a68df
Wael <12910823+WaelDev@users.noreply.github.com>
2022-04-25 15:46:31 +0100

    add BlocProvider to AppRouter

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,13 +1,34 @@
+import 'package:breaking_bad/business_logic/cubit/characters_cubit.dart';
+import 'package:breaking_bad/data/repositories/characters_repository.dart';
+import 'package:breaking_bad/data/web_services/characters_web_services.dart';
+import 'package:breaking_bad/presentation/screens/character_details_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'constants/strings.dart';
 import 'presentation/screens/characters_screen.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => charactersCubit,
+            child: const CharactersScreen(),
+          ),
+        );
+      case characterDetails:
+        return MaterialPageRoute(
+            builder: (_) => const CharacterDetailsScreen());
     }
     return null;
   }
